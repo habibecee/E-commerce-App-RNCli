f703577c2c0f4f01044b68b3b9caf4942a7d635e
habibecee <habibe.ce1996@gmail.com>
2023-05-15 14:22:10 +0300

    navigation-added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import axios from 'axios';
 import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import axios from 'axios';
 import {
+  Alert,
   Button,
   FlatList,
   SafeAreaView,
@@ -68,6 +70,75 @@ function App(): JSX.Element {
     setProducts(axiosResponse.data);
   };
 
+  const AddedProducts = () => {
+    axiosInstance
+      .post('products', {
+        // id: 4, --> POST İSTEĞİNDE ID VERİLMEMELİDİR!
+        title: 'Samsung ',
+        description:
+          'Samsung Galaxy S21 Ultra 5G 256 GB (Samsung Türkiye Garantili)',
+        price: 16499,
+        discountPercentage: 10,
+        rating: 4.5,
+        stock: 100,
+        brand: 'Samsung',
+        category: 'Telefon',
+        thumbnail:
+          'https://m.media-amazon.com/images/I/61O-QykQ7LL._AC_UY218_.jpg',
+        images: [
+          'https://m.media-amazon.com/images/I/61O-QykQ7LL._AC_UY218_.jpg',
+          'https://m.media-amazon.com/images/I/61O-QykQ7LL._AC_UY218_.jpg',
+          'https://m.media-amazon.com/images/I/61O-QykQ7LL._AC_UY218_.jpg',
+          'https://m.media-amazon.com/images/I/61O-QykQ7LL._AC_UY218_.jpg',
+        ],
+      })
+      .then(response => {
+        // console.log('response', response.status, response.data);
+        if (response.status === 201) {
+          AxiosProducts();
+        }
+      });
+  };
+
+  const UpdateProducts = () => {
+    axiosInstance
+      .put('products/3', {
+        // PUT İSTEĞİNDE ID VERİLMESİ GEREKİR!
+        id: 3,
+        title: 'Samsung Universe 9 Plus',
+        description:
+          "Samsung's new variant which goes beyond Galaxy to the Universe",
+        price: 14321,
+        discountPercentage: 15.46,
+        rating: 4.09,
+        stock: 36,
+        brand: 'Samsung',
+        category: 'smartphones',
+        thumbnail: 'https://i.dummyjson.com/data/products/3/thumbnail.jpg',
+        images: ['https://i.dummyjson.com/data/products/3/1.jpg'],
+      })
+      .then(response => {
+        // console.log('response', response.status, response.data);
+        if (response.status === 200) {
+          AxiosProducts();
+        }
+      });
+  };
+
+  const DeleteProducts = (productId: number) => {
+    axiosInstance
+      .delete(`products/${productId}`)
+      .then(response => {
+        console.log('response', response.status, response.data);
+        if (response.status === 200) {
+          AxiosProducts();
+        }
+      })
+      .catch(error => {
+        return Alert.alert('Error', 'Product not found!');
+      });
+  };
+
   useEffect(() => {
     // fetchProducts();
     AxiosProducts();
@@ -88,6 +159,12 @@ function App(): JSX.Element {
               <View>
                 <Button title="Show Data" onPress={() => AxiosProducts()} />
                 <Button title="Hide Data" onPress={() => setProducts([])} />
+                <Button title="Add Product" onPress={() => AddedProducts()} />
+                <Button title="Edit Product" onPress={() => UpdateProducts()} />
+                <Button
+                  title="Delete Product"
+                  onPress={() => DeleteProducts(1)}
+                />
               </View>
             );
           }}
@@ -101,7 +178,10 @@ function App(): JSX.Element {
           renderItem={props => {
             return (
               <View style={{paddingVertical: 10}}>
-                <Text> {props.item.title} </Text>
+                <Text>
+                  {' '}
+                  {props.item.title} -- {props.item.price}{' '}
+                </Text>
               </View>
             );
           }}
